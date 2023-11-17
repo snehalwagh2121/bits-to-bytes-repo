0928a1664eefc9daa17aadfd3d0c843307c927db
Harley <37466168+snehalwagh2121@users.noreply.github.com>
2023-11-17 11:38:01 +0530

    SPRING-79 - Docker commands add 1

--- src/devops/docker.js
+++ src/devops/docker.js
@@ -17,6 +17,12 @@ import productServiceApiHitwithDocker from './images/docker/productServiceApiHit
 import productServiceContainerLogs from './images/docker/productServiceContainerLogs.PNG';
 import productServiceDependency from './images/docker/productServiceDependency.PNG';
 import productServiceDockerfile from './images/docker/productServiceDockerfile.PNG';
+import attachCommand from './images/docker/attachCommand.PNG';
+import commitCommand from './images/docker/commitCommand.PNG';
+import psCommand from './images/docker/psCommand.PNG';
+import psaCommand from './images/docker/psaCommand.PNG';
+import inspectCommand from './images/docker/inspectCommand.PNG';
+import inspectCommand2 from './images/docker/inspectCommand2.PNG';
 
 function Docker() {
     return (
@@ -66,6 +72,33 @@ function Docker() {
                         <p><span>Logs: </span></p>
                         <p><span ><img className='sec-image' src={orderServiceContainerLogs} alt='orderServiceContainerLogs'  /></span></p>
                         <p><span ><img className='sec-image' src={productServiceContainerLogs} alt='productServiceContainerLogs'  /></span></p>
+                    </div>
+                </div>
+                <div className='heading-2'>Docker commands</div>
+                <div className='section-content'>
+                    <div className='heading 4'>
+                        <p><span><span className='bold'>ATTACH</span> This command is used to attach you terminal's std input, output and error to a running container using the container's Id or name. This allows to connect to the running container interactively. </span></p>
+                        <p><span className='bold-italics'>docker attach container_name </span></p>
+                        <p><span ><img className='sec-image' src={attachCommand} alt='attachCommand'  /></span></p>
+                        <p><span>Although pressing ctrl-C is stopping the whole container. Also while running the container use -dit.</span></p> 
+                        <p><span className='bold-italics'>docker run -ditp 9010:9010 --name order-container order-service </span></p>
+
+                        <p><span><span className='bold'>COMMIT</span> This command is used to create a new image based on the changes made to a container. It allows you to save the current state of a container as a new image. </span></p>
+                        <p><span className='bold-italics'>docker commit container_name </span> OR <span className='bold-italics'>docker commit container_id </span></p>
+                        <p><span ><img className='sec-image' src={commitCommand} alt='commitCommand'  /></span></p>
+                        <p><span>While it can be used to create an image that is similar to another one, it might not be the most recommended way to create replicas or ensure consistency across environments.</span></p> 
+
+                        <p><span><span className='bold'>PS</span> This command is used to list all the running containers. </span></p>
+                        <p><span className='bold-italics'>docker ps </span></p>
+                        <p><span ><img className='sec-image' src={psCommand} alt='psCommand'  /></span></p>
+                        <p><span>To see all the containers present even the ones which are not running.</span></p> 
+                        <p><span className='bold-italics'>docker ps -a </span></p>
+                        <p><span ><img className='sec-image' src={psaCommand} alt='psaCommand'  /></span></p>
+
+                        <p><span><span className='bold'>INSPECT</span> is used to obtain detailed information about Docker objects such as containers, images, volumes, and networks. It provides a JSON-formatted output that includes a wealth of information about the specified Docker object. The information retrieved can be useful for various purposes, including troubleshooting, scripting, and understanding the configuration of Docker resources.</span></p>
+                        <p><span className='bold-italics'>docker inspect container_name</span> OR <span className='bold-italics'>docker inspect image_name</span> OR <span className='bold-italics'>docker inspect network_name</span>  OR <span className='bold-italics'>docker inspect volume_name</span></p>
+                        <p><span ><img className='sec-image' src={inspectCommand} alt='inspectCommand'  /></span></p>
+                        <p><span ><img className='sec-image' src={inspectCommand2} alt='inspectCommand2'  /></span></p>
                     </div>
                 </div>
             </div>
